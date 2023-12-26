01cfeea6bd9d2145919cc007bd918904ff8aacd9
wangguoan408 <64453468+wangguoan408@users.noreply.github.com>
2023-12-26 15:53:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  return new Response("Hello world!");
+  return new Response("Hello you!");
 };
 
 serve(handleRequest, { port: 7777 });
